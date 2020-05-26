af50e06d34a2ef9fb88f7acc4a4106d8ca93269c
hhimanshu <harit.himanshu@yahoo.in>
2020-05-26 16:16:50 -0700

    Atualizado 123

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,13 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
+public class App
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
     }
-}
