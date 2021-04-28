b138ca91dacb37774427e8e55637245b03bf7079
Lucas Pereira <lucas.p.jorge13@gmail.com>
2021-04-28 17:22:14 -0300

    Atualizado 123

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -15,23 +15,22 @@ public class BestLoanRates {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
-        System.out.println("Hello "+ name);
+        System.out.println("Hello " + name);
 
         System.out.println("Enter the loan term (in years)");
         int loanTermInYears = scanner.nextInt();
         float bestRate = getRates(loanTermInYears);
-        if(bestRate == 0.0f) {
-            System.out.println("No available rates for terms: " + loanTermInYears + " years");
+        if (bestRate == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
         } else {
             System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
         }
-        scanner.close();
     }
 
     public static float getRates(int loanTermInYears) {
-       if(bestRates.containsKey(loanTermInYears)) {
-           return bestRates.get(loanTermInYears);
-       }
-       return 0.0f;
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;
     }
 }
